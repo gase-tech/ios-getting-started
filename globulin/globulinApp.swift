6e2e7a49059d94021ec7b52f351565229c9fc9c9
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2021-09-28 19:18:58 +0300

    Initial Commit

---
+++ globulin/globulinApp.swift
@@ -0,0 +1,17 @@
+//
+//  globulinApp.swift
+//  globulin
+//
+//  Created by Ömrüm Baki Temiz on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct globulinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
